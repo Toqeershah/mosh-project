8b8f0a5f83c65b161dda06e3b2f0e98053c93ca5
Toqeershah <77346481+Toqeershah@users.noreply.github.com>
2023-12-20 14:27:58 +0500

    adding toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,103 +1,45 @@
-// "use client";
-
-// import { Issue, User } from "@prisma/client";
-// import { Select } from "@radix-ui/themes";
-// import { useQuery } from "@tanstack/react-query";
-// import axios from "axios";
-// import {Skeleton} from '@/app/components'
-
-// const AssigneeSelect = ({issue}: {issue: Issue}) => {
-//   const {data: users, error, isLoading} = useQuery<User[]>({
-//     queryKey: ['users'],
-//     queryFn: () => axios.get('/api/users').then(res => res.data),
-//     staleTime: 60 * 1000,
-//     retry: 3
-//   })
-
-//   if (isLoading) return <Skeleton />;
-
-//   if (error) return null;
-
-
-//   return (
-//     <Select.Root onValueChange={(userId) => {
-//       axios.patch('/api/issues/' + issue.id, {assignedToUserId: userId || null})
-//     }}>
-//       <Select.Trigger placeholder="Assign..." />
-//       <Select.Content>
-//         <Select.Group>
-//           <Select.Label>Suggestions</Select.Label>
-//           <Select.Item value="">Unassigned</Select.Item>
-//           {users?.map((user) => (
-//             <Select.Item key={user.id} value={user.id}>
-//               {user.name}
-//             </Select.Item>
-//           ))}
-//         </Select.Group>
-//       </Select.Content>
-//     </Select.Root>
-//   );
-// };
-
-// export default AssigneeSelect;
-
 "use client";
 
-import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import toast, { Toaster } from "react-hot-toast";
+import {Skeleton} from '@/app/components'
+import toast from "react-hot-toast";
 
-const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const { data: users, error, isLoading } = useUsers();
+const AssigneeSelect = ({issue}: {issue: Issue}) => {
+  const {data: users, error, isLoading} = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000,
+    retry: 3
+  })
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
-  const assignIssue = (userId: string) => {
-    axios
-      .patch("/api/issues/" + issue.id, {
-        assignedToUserId: userId || null,
-      })
-      .catch(() => {
-        toast.error("Changes could not be saved.");
-      });
-  };
 
   return (
-    <>
-      <Select.Root
-        // defaultValue={issue.assignedToUserId || ""}
-        onValueChange={assignIssue}
-      >
-        <Select.Trigger placeholder="Assign..." />
-        <Select.Content>
-          <Select.Group>
-            <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="">Unassigned</Select.Item>
-            {users?.map((user) => (
-              <Select.Item key={user.id} value={user.id}>
-                {user.name}
-              </Select.Item>
-            ))}
-          </Select.Group>
-        </Select.Content>
-      </Select.Root>
-      <Toaster />
-    </>
+    <Select.Root onValueChange={(userId) => {
+      axios.patch('/api/issues/' + issue.id, {assignedToUserId: userId || null}).catch(() => {
+        toast.error("Changes could not be saved.")
+      })
+    }}>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="">Unassigned</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
   );
 };
-
-const useUsers = () =>
-  useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () =>
-      axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3,
-  });
 
 export default AssigneeSelect;
